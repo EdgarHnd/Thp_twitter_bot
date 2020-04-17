3d4651a29f964322dd582e0e45dcb61b699ac388
Haond <edgar.haond@gmail.com>
2020-04-17 11:42:26 +0200

    don't know what's new

--- lib/app.rb
+++ lib/app.rb
@@ -14,3 +14,34 @@ end
  def login_twitter
     return client
 end
+
+
+#puts client.user("lekillerforever")
+
+puts client.search("Troyes", lang: "fr").first.text 
+
+def client.get_all_tweets(user)
+        collect_with_max_id do |max_id|
+          options = {count: 1, include_rts: false}
+          options[:max_id] = max_id unless max_id.nil?
+          user_timeline(user, options)
+        end
+end
+
+#tweets_alex = client.get_all_tweets("lekillerforever")
+
+#tweets_alex.each do |t|
+       # puts client.status(t).text
+#end
+
+#client.follow("masterchuck111")
+
+#client.user_timeline("lekillerforever").each do |t|
+ #       puts client.status(t).text
+#end
+
+follow_paul = client.followers("masterchuck111")
+
+follow_paul.each do |f|
+        puts f.screen_name
+end
